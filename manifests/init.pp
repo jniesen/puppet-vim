a33c183f9446d4381f78e2503717ea5676a5bb9b
Jonathan Niesen <jon.niesen@gmail.com>
2015-03-16 08:20:14 -0400

    fix(init.pp): use pip to install mercurial and docutils

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,17 +18,16 @@ class vim {
   package { 'vim':
     require => Package['mercurial']
   }
+
   # Install mercurial since the vim brew package don't satisfy the requirement (vim is fetched using $ hg)
-  package { 'mercurial':
-    ensure   => install,
-    require  => Package['docutils'],
-    provider => pip,
+  python::pip { 'mercurial':
+    virtualenv => '/opt/boxen/homebrew',
   }
+
   # docutils is required by mercurial.
   # https://github.com/boxen/puppet-vim/issues/12
-  package { 'docutils':
-    ensure   => installed,
-    provider => pip,
+  python::pip { 'docutils':
+    virtualenv => '/opt/boxen/homebrew',
   }
 
   file { [$vimdir,
